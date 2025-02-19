ee96d59384eb1933bb481f421e4208bacd0fb0d5
WasaPumpkin <andycarsan10@gmail.com>
2025-02-19 17:16:31 -0500

    vite-config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     '/api': {
+//       target: "http://localhost:4000",
+//       changeOrigin: true
+//     }
+//   }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    '/api': {
-      target: "http://localhost:4000",
-      changeOrigin: true
-    }
-  }
-})
+    proxy: {
+      // Add the proxy configuration here
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        
+      },
+    },
+  },
+});
